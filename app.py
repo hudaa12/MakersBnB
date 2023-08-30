d569a2544134e2c7e42c7c838ebd5981e1e858e9
David <drop6230@hotmail.co.uk>
2023-08-30 15:23:09 +0100

    html space

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, request, render_template
 from lib.database_connection import get_flask_database_connection
+from lib.space import Space
+from lib.spaces_repository import SpacesRepository
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -16,6 +18,26 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+@app.route('/space', methods=['GET'])
+def get_space():
+    return render_template('index_space.html')
+
+@app.route('/space', methods=['POST'])
+def list_space():
+    connection = get_flask_database_connection(app)
+    repository = SpacesRepository(connection) 
+    name = request.form['space_name']
+    description = request.form['description']
+    price = request.form['price']
+    available_from = request.form['available_from']
+    available_to = request.form['available_to']
+    user_id = request.form['user_id']
+
+    space = Space(None, name, description, price, available_from, available_to, user_id) 
+    repository.create(space)
+
+    return render_template('index_space.html')
 
 
 # These lines start the server if you run this file directly
