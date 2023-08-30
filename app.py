13f18056c3e63c9745c41035582b4bbc7f015a8c
ami <amiday22@gmail.com>
2023-08-30 13:23:35 +0100

    Added login route and logout route

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, session
 from lib.database_connection import get_flask_database_connection
+from flask_session import Session
 
 # Create a new Flask app
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # == Your Routes Here ==
 
@@ -15,11 +19,23 @@ app = Flask(__name__)
 
 @app.route('/index', methods=['GET'])
 def get_index():
+    if not session.get("name"):
+        return redirect("/login")
     return render_template('index.html')
 
 @app.route('/login', methods=['GET'])
 def get_login():
     return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def post_login():
+    session["name"] = request.form.get("User name")
+    return redirect("/spaces")
+
+@app.route("/logout")
+def logout():
+    session["name"] = None
+    return redirect("index.html")
 
 
 # These lines start the server if you run this file directly
