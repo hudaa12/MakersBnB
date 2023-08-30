4e29b17de8d2f109c4126fe2fc287168ace97873
Jake Siney <jakesiney@gmail.com>
2023-08-30 09:22:46 +0100

    Finished booking tests

--- lib/booking_repository.py
+++ lib/booking_repository.py
@@ -5,6 +5,12 @@ class BookingRepository:
     def __init__(self, connection):
         self._connection = connection
 
+    def create_booking(self, booking):
+        rows = self._connection.execute(
+            'INSERT INTO bookings (booking_date, confirmed, booked_by, space_id) VALUES (%s, %s, %s, %s) RETURNING id', [booking.booking_date, booking.confirmed, booking.booked_by, booking.space_id])
+        booking.id = rows[0]['id']
+        return None
+
     def all(self):
         rows = self._connection.execute(
             'SELECT * FROM bookings'
@@ -18,8 +24,15 @@ class BookingRepository:
 
     def find(self, id):
         rows = self._connection.execute(
-            'SELECT FROM bookings WHERE id = %s',
+            'SELECT * from bookings WHERE id = %s',
             [id]
         )
         row = rows[0]
         return Booking(row['id'], row['booking_date'], row['confirmed'], row['booked_by'], row['space_id'])
+
+    def delete_booking(self, id):
+        self._connection.execute(
+            'DELETE FROM bookings WHERE id = %s',
+            [id]
+        )
+        return None
