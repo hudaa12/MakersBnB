a4dc48c51e7455d5eba26ae98d5bdf14103c52d2
Jake Siney <jakesiney@gmail.com>
2023-08-30 13:39:17 +0100

    Update create user page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from lib.database_connection import get_flask_database_connection
+from lib.users_repository import UsersRepository
+from lib.users import Users
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -13,9 +15,22 @@ app = Flask(__name__)
 #   ; open http://localhost:5000/index
 
 
-@app.route('/index', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_index():
     return render_template('index.html')
+
+
+@app.route('/index', methods=['POST'])
+def new_user_created():
+    connection = get_flask_database_connection(app)
+    repository = UsersRepository(connection)
+
+    email = request.form['email']
+    password = request.form['password']
+    user = Users(None, email, password)
+    repository.new_user_created(user)
+
+    return render_template('/index.html')
 
 
 # These lines start the server if you run this file directly
