fc26ba5c16940d924993e4e0095f66c42d2ab868
Jake Siney <jakesiney@gmail.com>
2023-09-01 13:49:43 +0100

    Bug fix

--- lib/space.py
+++ lib/space.py
@@ -1,6 +1,6 @@
 
 class Space:
-    def __init__(self,id, name, description, price, avail_from, avail_to,profile_photo, user_id):
+    def __init__(self, id, name, description, price, avail_from, avail_to, profile_photo, user_id):
         self.id = id
         self.name = name
         self.description = description
@@ -10,7 +10,7 @@ class Space:
         self.profile_photo = profile_photo
         self.user_id = user_id
 
-    def __eq__(self,other):
+    def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
     def __repr__(self):
